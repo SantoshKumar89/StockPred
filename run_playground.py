e485979c6fa773f4522b9960381a5dc46f798e2f
Santosh Kumar <santosh89ks@gmail.com>
2024-08-04 20:40:25 +0530

    passing args

--- run_playground.py
+++ run_playground.py
@@ -2,8 +2,10 @@
 
 import subprocess
 
+
 def run_script_with_args(script_path, arg1, arg2):
-    command = ['python3', script_path, arg1, arg2]
+
+    command = ["python3", script_path, arg1, arg2]
     print(f"Running command: {' '.join(command)}")  # Debugging line
     result = subprocess.run(command, capture_output=True, text=True)
 
@@ -14,12 +16,32 @@ def run_script_with_args(script_path, arg1, arg2):
         print("Error executing script:")
         print(result.stderr)
 
-def main():
-    script_path = 'playground_hourly_high_multi_input_forecast.py'
-    dynamic_arg1 = 'Hello'
-    dynamic_arg2 = 'World'
 
-    run_script_with_args(script_path, dynamic_arg1, dynamic_arg2)
+def main():
+    script_path = "playground_hourly_high_multi_input_forecast.py"
+    stockname = "NIFTY 50"
+    symbol = "^NSEI"
+    start_date = "2022-08-25"
+    start_time = "00:00:00"
+    end_date = "2024-07-25"
+    end_time = "10:15:00"
+    interval = "1h"
+    input_sequence_length = 35
+    output_sequence_length = 1
+
+    run_script_with_args(
+        script_path,
+        stockname,
+        symbol,
+        start_date,
+        start_time,
+        end_date,
+        end_time,
+        interval,
+        input_sequence_length,
+        output_sequence_length,
+    )
+
 
 if __name__ == "__main__":
     main()
